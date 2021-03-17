39286c4e463bcf5435a1ee5fdac469935c053d1f
xxxVitoxxx <bird06291994@gmail.com>
2021-03-18 00:37:34 +0800

    first commit

---
+++ Desktop/python/marathon_work/Day001_Sample.py
@@ -0,0 +1,4 @@
+from urllib.request import urlretrieve
+
+urlretrieve ("https://www.w3.org/TR/PNG/iso_8859-1.txt", "./1.txt")
+urlretrieve ("https://www.w3.org/TR/PNG/iso_8859-1.txt", "./data/2.txt")
